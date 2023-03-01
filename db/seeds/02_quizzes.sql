dd7764ebd11916ee814378548fe382d531393d2e
marcuszcoding <marcuszcoding@gmail.com>
2023-03-01 15:25:48 -0700

    add seeds for quiz 1

---
+++ db/seeds/02_quizzes.sql
@@ -0,0 +1,1 @@
+INSERT INTO quizzes(owner_id, title, public_listed, url) VALUES (1, 'Javascript', true, 'javascript')
---
+++ db/seeds/03_quiz_questions.sql
@@ -0,0 +1,7 @@
+-- QUIZ 1 Questions
+INSERT INTO quiz_questions(quiz_id, question) VALUES (1, `What does the "typeof" operator in JavaScript return?`)
+INSERT INTO quiz_questions(quiz_id, question) VALUES (1, `Which of the following is a valid way to declare a variable in JavaScript?`)
+INSERT INTO quiz_questions(quiz_id, question) VALUES (1, `What does the "this" keyword in JavaScript refer to?`)
+INSERT INTO quiz_questions(quiz_id, question) VALUES (1, `What is the difference between == and === in JavaScript?`)
+INSERT INTO quiz_questions(quiz_id, question) VALUES (1, `Which of the following is not a valid JavaScript loop?`)
+-- END QUIZ 1 QUESTIONS
