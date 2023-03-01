9ee97887fd8913ae96a483deaed10820d7435648
marcuszcoding <marcuszcoding@gmail.com>
2023-03-01 13:29:07 -0700

    add tables to db

---
+++ db/schema/06_quiz_results.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS trivia_results CASCADE;
+
+CREATE TABLE trivia_results (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  score INTEGER DEFAULT 0,
+  -- date_completed TIMESTAMP NOT NULL
+);
