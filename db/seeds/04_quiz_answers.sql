c3b8d8a1c73f6d362e1ff9d703a8a8a3d6bc78d6
marcuszcoding <marcuszcoding@gmail.com>
2023-03-01 16:10:32 -0700

    fix schema/seed erros and mismatched names

--- db/seeds/04_quiz_answers.sql
+++ db/seeds/04_quiz_answers.sql
@@ -1,28 +1,28 @@
 -- Test 1 Javascript Questions
-INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (1, 'The data type of a variable', true);
-INSERT INTO trivia_answers (question_id, answer) VALUES (1, 'The value of a variable');
-INSERT INTO trivia_answers (question_id, answer) VALUES (1, 'The size of a variable');
-INSERT INTO trivia_answers (question_id, answer) VALUES (1, 'The memory location of a variable');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (1, 'The data type of a variable', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (1, 'The value of a variable');
+INSERT INTO quiz_answers (question_id, answer) VALUES (1, 'The size of a variable');
+INSERT INTO quiz_answers (question_id, answer) VALUES (1, 'The memory location of a variable');
 
-INSERT INTO trivia_answers (question_id, answer) VALUES (2, 'var x = 5;');
-INSERT INTO trivia_answers (question_id, answer) VALUES (2, 'let y = 10;');
-INSERT INTO trivia_answers (question_id, answer) VALUES (2, 'cost z = 15;');
-INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (2, 'All of the above', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (2, 'var x = 5;');
+INSERT INTO quiz_answers (question_id, answer) VALUES (2, 'let y = 10;');
+INSERT INTO quiz_answers (question_id, answer) VALUES (2, 'cost z = 15;');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (2, 'All of the above', true);
 
-INSERT INTO trivia_answers (question_id, answer) VALUES (3, 'The current function');
-INSERT INTO trivia_answers (question_id, answer) VALUES (3, 'The global object');
-INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (3, 'The current object', true);
-INSERT INTO trivia_answers (question_id, answer) VALUES (3, 'The current class');
+INSERT INTO quiz_answers (question_id, answer) VALUES (3, 'The current function');
+INSERT INTO quiz_answers (question_id, answer) VALUES (3, 'The global object');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (3, 'The current object', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (3, 'The current class');
 
-INSERT INTO trivia_answers (question_id, answer) VALUES (4, 'There is no difference');
-INSERT INTO trivia_answers (question_id, answer) VALUES (4, '== compares value and type, while === only compares value');
-INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (4, '=== compares value and type, while == only compares value', true);
-INSERT INTO trivia_answers (question_id, answer) VALUES (4, 'None of the above');
+INSERT INTO quiz_answers (question_id, answer) VALUES (4, 'There is no difference');
+INSERT INTO quiz_answers (question_id, answer) VALUES (4, '== compares value and type, while === only compares value');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (4, '=== compares value and type, while == only compares value', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (4, 'None of the above');
 
-INSERT INTO trivia_answers (question_id, answer) VALUES (5, 'for');
-INSERT INTO trivia_answers (question_id, answer, is_correct) VALUES (5, 'switch', true);
-INSERT INTO trivia_answers (question_id, answer) VALUES (5, 'while');
-INSERT INTO trivia_answers (question_id, answer) VALUES (5, 'do-while');
+INSERT INTO quiz_answers (question_id, answer) VALUES (5, 'for');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (5, 'switch', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (5, 'while');
+INSERT INTO quiz_answers (question_id, answer) VALUES (5, 'do-while');
 -- End of Test 1 Javscript Questions
 
 
