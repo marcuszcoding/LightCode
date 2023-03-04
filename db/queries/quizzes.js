72811c9e5c26e751a6ad1baaa76d726bc3c57686
Domara Hawil <domara.hawil@hotmail.com>
2023-03-04 14:36:46 -0500

    quizzes route and query for quizzes

--- db/queries/quizzes.js
+++ db/queries/quizzes.js
@@ -5,7 +5,7 @@ const create = (newQuiz) => {
   const { owner_id, title, public_listed, url } = newQuiz;
   return db
     .query(
-      "INSERT INTO notes (user_id, content) VALUES ($1, $2) RETURNING *;",
+      "INSERT INTO quizzes (owner_id, title, public_listed, url) VALUES ($1, $2) RETURNING *;",
       [owner_id, title, public_listed, url]
     )
     .then((data) => data.rows[0]);
