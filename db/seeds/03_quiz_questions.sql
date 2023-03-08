00e0c27ed4a7734abb4d5a41e246b82be3bd125f
Domara Hawil <domara.hawil@hotmail.com>
2023-03-07 20:35:47 -0500

    created other questions for html and css quizzes

--- db/seeds/03_quiz_questions.sql
+++ db/seeds/03_quiz_questions.sql
@@ -5,7 +5,18 @@ INSERT INTO quiz_questions(quiz_id, question) VALUES (1, 'What does the "this" k
 INSERT INTO quiz_questions(quiz_id, question) VALUES (1, 'What is the difference between == and === in JavaScript?');
 INSERT INTO quiz_questions(quiz_id, question) VALUES (1, 'Which of the following is not a valid JavaScript loop?');
 -- END QUIZ 1 QUESTIONS
+INSERT INTO quiz_questions(quiz_id, question) VALUES (2, 'What does HTML stand for?');
+INSERT INTO quiz_questions(quiz_id, question) VALUES (2, 'What element is used to make stlyes in your HTML');
+INSERT INTO quiz_questions(quiz_id, question) VALUES (2, 'What is a hyperlink in HTML?');
+INSERT INTO quiz_questions(quiz_id, question) VALUES (2, 'What is HTML used for?');
+INSERT INTO quiz_questions(quiz_id, question) VALUES (2, 'What is the difference between the title and the h1 tag in HTML?');
 
+-- END QUIZ 2 QUESTIONS
+INSERT INTO quiz_questions(quiz_id, question) VALUES (3, 'What is the box model in CSS?');
+INSERT INTO quiz_questions(quiz_id, question) VALUES (3, 'What is the difference between padding and margin in CSS?');
+INSERT INTO quiz_questions(quiz_id, question) VALUES (3, 'How do you center an element horizontally in CSS?');
+INSERT INTO quiz_questions(quiz_id, question) VALUES (3, 'What is the float property in CSS?');
+INSERT INTO quiz_questions(quiz_id, question) VALUES (3, 'What is a CSS selector?');
 
-
+-- END QUIZ 3 QUESTIONS
 
--- db/seeds/04_quiz_answers.sql
+++ db/seeds/04_quiz_answers.sql
@@ -24,5 +24,62 @@ INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (5, 'switch', 
 INSERT INTO quiz_answers (question_id, answer) VALUES (5, 'while');
 INSERT INTO quiz_answers (question_id, answer) VALUES (5, 'do-while');
 -- End of Test 1 Javscript Questions
+----------------------------------------------------------------------------------------
+INSERT INTO quiz_answers (question_id, answer) VALUES (6, 'HyperText Market Language');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (6, 'HyperText Markup Language', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (6, 'Hightlight Text Markup Language');
+INSERT INTO quiz_answers (question_id, answer) VALUES (6, 'HyperType Markup Language');
 
 
+INSERT INTO quiz_answers (question_id, answer) VALUES (7, '<CSS> </CSS>');
+INSERT INTO quiz_answers (question_id, answer) VALUES (7, '<spam> </spam>');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (7, '<styles></styles>', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (7, '<p></p>');
+
+
+INSERT INTO quiz_answers (question_id, answer) VALUES (8, 'A hyperlink in HTML is a clickable link that allows users to navigate to other web pages');
+INSERT INTO quiz_answers (question_id, answer) VALUES (8, 'A hyperlink is HTML is a file that saves all data');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (8, 'A hyperlink in HTML creates a link to use in Javascript', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (8, 'A hyperlink is a link that is faster than other links because its hyper');
+
+
+INSERT INTO quiz_answers (question_id, answer) VALUES (9, 'It is used for creating the styles of a webpage');
+INSERT INTO quiz_answers (question_id, answer) VALUES (9, 'It is used for created the functions of a webpage');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (9, 'It is used for creating the structure of a webpage', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (9, 'All of the above');
+
+
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (10, 'The title tag is used to define the title of the HTML document, The h1 tag, on the other hand, is used to define the main heading of a web page', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (10, 'The title tag is used to define the url and the h1 is to make a paragraph');
+INSERT INTO quiz_answers (question_id, answer) VALUES (10, 'The title tag is used to creat a title for the project and the h1 is used to create styles');
+INSERT INTO quiz_answers (question_id, answer) VALUES (10, 'None of the above');
+-- END OF QUIZ 2 HTML
+----------------------------------------------------------------------------------------
+
+INSERT INTO quiz_answers (question_id, answer) VALUES (11, 'The box model is a HTML concept displayed as boxes');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (11, 'The box model is a CSS concept that defines how elements are displayed as boxes, including the content area, padding, border, and margin.', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (11, 'The Box model is a CSS concept that includes color, test, are, padding and border');
+INSERT INTO quiz_answers (question_id, answer) VALUES (11, 'None of the above');
+
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (12, 'Padding is the space between an elements content and its border, while margin is the space between an elements border and the adjacent elements or the browser window', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (12, 'Padding is the space between an HTML tag element and margin is the space between and elements border');
+INSERT INTO quiz_answers (question_id, answer) VALUES (12, 'Padding is the area around the border and margin is the area around the element');
+INSERT INTO quiz_answers (question_id, answer) VALUES (12, 'None of the above');
+
+INSERT INTO quiz_answers (question_id, answer) VALUES (13, 'To center an element horizontally you can use the space between property');
+INSERT INTO quiz_answers (question_id, answer) VALUES (13, 'To center an element in CSS you can use the flex-box property');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (13, 'To center an element horizontally in CSS, you can use the margin property and set its value to auto', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (13, 'To center an element you need to use the center property ');
+
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (14, 'The float property in CSS is used to align elements to the left or right of their container', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (14, 'The float property in CSS is used to align the up and down of the class container');
+INSERT INTO quiz_answers (question_id, answer) VALUES (14, 'The float property in CSS is used to center the element that is being targeted');
+INSERT INTO quiz_answers (question_id, answer) VALUES (14, 'The float property in CSS is used to align the HTML to the page');
+
+INSERT INTO quiz_answers (question_id, answer) VALUES (15, 'A CSS class selector is a targeted property that identifies a class');
+INSERT INTO quiz_answers (question_id, answer) VALUES (15, 'Only identifies a id');
+INSERT INTO quiz_answers (question_id, answer, is_correct) VALUES (15, 'A CSS selector is a pattern used to select and target specific HTML elements to apply styles to', true);
+INSERT INTO quiz_answers (question_id, answer) VALUES (15, 'All of the above');
+
+-- END OF QUIZ 3 CSS
+----------------------------------------------------------------------------------------
