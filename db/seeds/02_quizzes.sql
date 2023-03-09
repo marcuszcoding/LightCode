5ada5fc25e33b5fa4df90b57541f66910cc406ea
Domara Hawil <domara.hawil@hotmail.com>
2023-03-08 20:00:56 -0500

    update

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -2,4 +2,4 @@ INSERT INTO quizzes(owner_id, title, public_listed, url) VALUES (1, 'Javascript'
 
 INSERT INTO quizzes(owner_id, title, public_listed, url) VALUES (1, 'HTML', true, 'HTML');
 
-INSERT INTO quizzes(owner_id, title, public_listed, url) VALUES (1, 'CSS', true, 'CSS');
+INSERT INTO quizzes(owner_id, title, public_listed, url) VALUES (1, 'CSS', false, 'CSS');
